30fcc4fd51a32ee2fdea123de306a7df00c747cd
Rustem_Murzagaliyev <r_murzagaliyev@kbtu.kz>
2025-09-20 10:02:56 +0500

    lab3

--- lab3/i.cpp
+++ lab3/i.cpp
@@ -5,18 +5,26 @@ using namespace std;
 int binary_search_exact(vector<int>& a, int x) {
     int l = 0, r = (int)a.size() - 1;
     while (l <= r) {
-        int mid = l + (r - l) / 2;  // Avoids overflow
+        int mid = l + (r - l) / 2;  
         if (a[mid] == x) return mid;
         if (a[mid] < x) l = mid + 1;
         else r = mid - 1;
     }
-    return -1; // not found
+    return -1;
 }
 
 int main() {
-    vector<int> a = {3, 5, 8, 13, 18, 19, 21, 27, 32};
-    int x = 13;
+    int n;
+    cin >> n;
+    vector<int> a;
+    for(int i =0;i <n; i++){
+        int temp;
+        cin >> temp;
+        a.push_back(temp);
+    }
+    int x;
+    cin >> x;
     int idx = binary_search_exact(a, x);
-    cout << (idx != -1 ? "Found at index " + to_string(idx) : "Not found") << "\n";
+    cout << (idx != -1 ? "Yes" : "No");
     return 0;
 }
